42f97baa00dfea7f8c80a4fe485e7c68b957ec04
jsg0629 <tnrms513@gmail.com>
2022-05-15 03:45:52 +0900

    :sparkles: FEAT, CHORE: recoli로 input value관리, Serch컴포넌트 inpur icon 위치 수정

--- src/components/SerchInput/serch.tsx
+++ src/components/SerchInput/serch.tsx
@@ -2,12 +2,32 @@ import React from 'react'
 import style from './Serch.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { useRecoilState } from 'recoil'
+import { searchValue } from 'hooks/atoms'
 
 const Serch = (): JSX.Element => {
+  const [InputValue, setInputValue] = useRecoilState(searchValue)
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+  }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.currentTarget.value)
+  }
+
   return (
     <div className={style.serchInputWrraper}>
-      <FontAwesomeIcon icon={faMagnifyingGlass} className={style.serchIcon} />
-      <input className={style.serchInput} type='text' placeholder='Search' />
+      <form onSubmit={handleFormSubmit}>
+        <button type='submit'>
+          <FontAwesomeIcon icon={faMagnifyingGlass} className={style.serchIcon} />
+        </button>
+        <input
+          className={style.serchInput}
+          type='text'
+          value={InputValue}
+          placeholder='Search'
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   )
 }
